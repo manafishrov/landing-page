7b24afbfb122c52e65b2594dc52222b3f9a5b8a5
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-09-02 16:32:33 +0200

    Revert "feat: add tanstack query"
    This reverts commit 3dbabba7180fc94f7432ae55749e1e0a1a0ad75b.

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,12 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
 
 import Root from '@/routes/__root';
 
 import '@/styles/app.css';
 
-const queryClient = new QueryClient();
-
 const rootElement = document.getElementById('app')!;
 
 if (!rootElement.innerHTML) {
   const root = createRoot(rootElement);
-  root.render(
-    <QueryClientProvider client={queryClient}>
-      <Root />
-    </QueryClientProvider>,
-  );
+  root.render(<Root />);
 }
