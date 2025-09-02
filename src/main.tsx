3dbabba7180fc94f7432ae55749e1e0a1a0ad75b
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-09-02 16:31:58 +0200

    feat: add tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
 
 import Root from '@/routes/__root';
 
 import '@/styles/app.css';
 
+const queryClient = new QueryClient();
+
 const rootElement = document.getElementById('app')!;
 
 if (!rootElement.innerHTML) {
   const root = createRoot(rootElement);
-  root.render(<Root />);
+  root.render(
+    <QueryClientProvider client={queryClient}>
+      <Root />
+    </QueryClientProvider>,
+  );
 }
